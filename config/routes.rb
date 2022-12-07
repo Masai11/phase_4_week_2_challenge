ddefb27f3b35c3c5b2ada534f7a29c161731781d
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-12-07 07:30:26 +0300

    done with challenge

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :heroes
+  resources :powers
+  resources :hero_powers
+
+end
---
+++ db/migrate/20221207033455_add_power_to_hero_powers.rb
@@ -0,0 +1,6 @@
+class AddPowerToHeroPowers < ActiveRecord::Migration[7.0]
+  def change
+
+    add_reference :hero_powers, :power, null: false, foreign_key: true
+  end
+end
